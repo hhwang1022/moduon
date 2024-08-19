b2b85c45f6fa5f231dce27c201aeeb39378f0101
JinHyeongSeo <jh4clover@naver.com>
2024-08-19 23:52:16 +0900

    #38 투표댓글 생성 코드추가

--- src/main/java/com/springboot/board/balancegame/service/BalanceGameReplyService.java
+++ src/main/java/com/springboot/board/balancegame/service/BalanceGameReplyService.java
@@ -1,8 +1,8 @@
 package com.springboot.board.balancegame.service;
 
-import com.springboot.board.balancegame.entity.BalanceGame;
 import com.springboot.board.balancegame.entity.BalanceGameReply;
 import com.springboot.board.balancegame.repository.BalanceGameReplyRepository;
+import com.springboot.board.balancegame.repository.BalanceGameRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,9 +10,12 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class BalanceGameReplyService {
     private final BalanceGameReplyRepository balanceGameReplyRepository;
+    private final BalanceGameRepository balanceGameRepository;
 
-    public BalanceGameReplyService(BalanceGameReplyRepository balanceGameReplyRepository) {
+    public BalanceGameReplyService(BalanceGameReplyRepository balanceGameReplyRepository,
+                                   BalanceGameRepository balanceGameRepository) {
         this.balanceGameReplyRepository = balanceGameReplyRepository;
+        this.balanceGameRepository = balanceGameRepository;
     }
 
     public BalanceGameReply createBalanceGameReply(BalanceGameReply balanceGameReply) {
