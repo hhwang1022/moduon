fa565727fb708eff4c489141e49f77d21ddbd680
JinHyeongSeo <jh4clover@naver.com>
2024-08-21 01:44:21 +0900

    #47 날짜 확인 메서드 변경

--- src/main/java/com/springboot/board/balancegame/service/BalanceGameService.java
+++ src/main/java/com/springboot/board/balancegame/service/BalanceGameService.java
@@ -77,17 +77,10 @@ public class BalanceGameService {
 		return findBalanceGame;
 	}
 
-	public BalanceGame.BalanceGameStatus compareToLocalDateTime(LocalDateTime EndTime) {
-		switch(LocalDateTime.now().compareTo(EndTime)) {
-			// 지금이 endTime보다 뒤일경우
-			case 1:
-				return BalanceGame.BalanceGameStatus.INACTIVE;
-			// 지금이 endTime과 같거나 앞에 있을경우
-			case 0:
-				return BalanceGame.BalanceGameStatus.ACTIVE;
-			case -1:
-				return BalanceGame.BalanceGameStatus.ACTIVE;
-        }
-		return BalanceGame.BalanceGameStatus.ACTIVE;
+	public boolean isAfterNow(LocalDateTime EndTime) {
+		if (EndTime.isAfter(LocalDateTime.now())) {
+			return true;
+		}
+		return false;
 	}
 }
