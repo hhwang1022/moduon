e32ffa8d08f2f17e7bf06656e3cd3844698cd0ab
JinhyeongSeo <jh4clover@naver.com>
2024-08-21 15:38:42 +0900

    #36 sharePostToShare 추가

--- src/main/java/com/springboot/share/mapper/ShareMapper.java
+++ src/main/java/com/springboot/share/mapper/ShareMapper.java
@@ -1,4 +1,26 @@
 package com.springboot.share.mapper;
 
+import com.springboot.board.balancegame.entity.BalanceGame;
+import com.springboot.member.entity.Member;
+import com.springboot.share.dto.ShareDto;
+import com.springboot.share.entity.Share;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedSourcePolicy = ReportingPolicy.IGNORE)
 public interface ShareMapper {
+    default Share sharePostToShare(ShareDto.Post requestBody) {
+        Member member = new Member();
+        member.setMemberId(requestBody.getMemberId());
+
+        BalanceGame balanceGame = new BalanceGame();
+        balanceGame.setBalanceGameId(requestBody.getBalanceGameId());
+
+        Share share = new Share();
+        share.setMember(member);
+        share.setBalanceGame(balanceGame);
+        share.setShareType(requestBody.getShareType());
+
+        return share;
+    }
 }
