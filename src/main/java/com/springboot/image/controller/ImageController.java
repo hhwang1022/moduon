043d7fa89b3d79f4ebbe83c809a078a84a65cb2d
hhwang1022 <hjd2110@naver.com>
2024-08-21 15:47:17 +0900

    modifyimagecontroller

--- src/main/java/com/springboot/image/controller/ImageController.java
+++ src/main/java/com/springboot/image/controller/ImageController.java
@@ -1,10 +1,28 @@
-package com.springboot.img.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/imgs")
-public class ImgController {
-
-}
+//package com.springboot.image.controller;
+//import com.springboot.image.dto.ImageDto;
+//import com.springboot.image.service.ImageService;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.MediaType;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//import org.springframework.web.multipart.MultipartFile;
+//
+//import javax.validation.Valid;
+//import java.net.URI;
+//
+//@RestController
+//@RequestMapping("/images")
+//public class ImageController {
+//
+////    final private ImageService imageService;
+////
+////    public ImageController(ImageService imageService) {
+////        this.imageService = imageService;
+////    }
+////
+////    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+////    public ResponseEntity postImage(@Valid @RequestPart ImageDto imageDto,
+////                                    @RequestPart MultipartFile multipartFile){
+////        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+////    }
+//}
