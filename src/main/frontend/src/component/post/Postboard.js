e4be2ba87ffb87434d3008b8f3480234c4ca8348
hhwang1022 <hjd2110@naver.com>
2024-08-21 12:04:56 +0900

    addnewhot

--- src/main/frontend/src/component/post/Postboard.js
+++ src/main/frontend/src/component/post/Postboard.js
@@ -2,18 +2,25 @@ import InstarIcon from '../1020/InstarIcon';
 import './PostlistItem.css';
 import React, { useState, useEffect } from 'react';
 
-const PostlistItem = ({ post, generation }) => {
+const Postboard = ({ generation }) => {
 
-    let getDayDiffDay = (createsat) =>
-        (new Date() - new Date(createsat)) / (1000 * 3600);
-
-    const simpleday = (day) =>
-    (
-        // yyyy-mm-dd
-        `${new Date(day).getFullYear()}-
-          ${(new Date(day).getMonth() + 1).toString().padStart(2, '0')}-
-          ${new Date(day).getDate().toString().padStart(2, '0')}`
-    );
+    const Page = () => {
+        if(currentindex === 0){
+            return (
+                <Postlist generation={"0010"} />
+            );
+        }
+        else if(currentindex === 1){
+            return (
+                <Photolist generation={"0010"} />
+            );
+        }
+        else{
+            return (
+                <Currentvote_board generation={"0010"} />
+            );
+        }
+      };
 
     return (
         (generation === "1020" ?
@@ -23,21 +30,19 @@ const PostlistItem = ({ post, generation }) => {
                     <div className={'postlist' + generation + 'body'}>
                         <div>
                             <td className={'postlist' + generation + 'text'}>{post.nickname + "\u00a0"}</td>
-                            <td className={'postlist' + generation + 'date'}>{simpleday(post.createsat) + "\u00a0"}</td>
-                            {getDayDiffDay(post.createsat) <= 2.0 ? <td className={'postitem' + generation + 'newtext'}>new</td> : <></>}
-                            {post.like >= 10 ? <td className={'postitem' + generation + 'hottext'}>hot</td> : <></>}
+                            <td className={'postlist' + generation + 'date'}>{post.createsat + "\u00a0"}</td>
+                            <td className={'postitem' + generation + 'newtext'}>new</td>
                         </div>
                         <td className={'postlist' + generation + 'text'}><button>{post.title}</button></td>
                         <div>
-                            <td className={'postlist' + generation + 'text'}><img src='https://cdn-icons-png.flaticon.com/512/1077/1077057.png' height={16} width={16} />{post.view}</td>
-                            <td className={'postlist' + generation + 'text'}><img src='https://cdn-icons-png.flaticon.com/512/6611/6611465.png' height={16} width={16} />{post.like}</td>
+                            <td className={'postlist' + generation + 'text'}><img src='https://cdn-icons-png.flaticon.com/512/1077/1077057.png' height={16} width={16}/>{post.view}</td>
+                            <td className={'postlist' + generation + 'text'}><img src='https://cdn-icons-png.flaticon.com/512/6611/6611465.png' height={16} width={16}/>{post.like}</td>
                         </div>
                     </div>
                 </div>
             </div>
             : <tr className={'postlist' + generation + 'line'} height="34">
-                {getDayDiffDay(post.createsat) <= 2.0 ? <td className={'postitem' + generation + 'newtext'}>new</td> : <></>}
-                {post.like >= 10 ? <td className={'postitem' + generation + 'hottext'}>hot</td> : <></>}
+                <td className={'postitem' + generation + 'newtext'}>new</td>
                 <td className={'postlist' + generation + 'text'}><button>{post.title}</button></td>
                 <td className={'postlist' + generation + 'text'}>{post.nickname}</td>
                 <td className={'postlist' + generation + 'text'}>{post.createsat}</td>
@@ -49,4 +54,4 @@ const PostlistItem = ({ post, generation }) => {
     );
 };
 
-export default PostlistItem;
+export default Postboard;
