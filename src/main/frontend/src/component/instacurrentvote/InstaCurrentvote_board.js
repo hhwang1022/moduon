a084cc33dfcc40e2ce8ea4fa10431b49cf1603d8
wooil6 <wooilkf@naver.com>
2024-08-22 10:55:51 +0900

    1020 vote

--- src/main/frontend/src/component/instacurrentvote/InstaCurrentvote_board.js
+++ src/main/frontend/src/component/instacurrentvote/InstaCurrentvote_board.js
@@ -1,33 +1,69 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './InstaCurrentvote_board.css';
 import InstaBalancebar from './InstaBalancebar';
 import Balancegame_commentlist from '../currentvote/Balancegame_commentlist';
 
 const InstaCurrentvote_board = () => {
+
+    const [voteTitle, setVoteTitle] = useState('');
+    const [voteImage1, setVoteImage1] = useState('');
+    const [voteImage2, setVoteImage2] = useState('');
+    const [voteItem1, setVoteItem1] = useState('');
+    const [voteItem2, setVoteItem2] = useState('');
     const [searchkeyword, setsearchkeyword] = useState('');
+
+    let accessToken = window.localStorage.getItem('accessToken');
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://127.0.0.1:8080/balancegames/this-week?'
+                + 'page=' + 1 + '&size=' + 10 + '&generation=' + 1020, {
+                headers: { Authorization: `Bearer ${accessToken}` }
+                });
+
+                const data = response.data.data;
+                    if(data && data.length > 0) {
+                        const voteData = data[0];
+                        setVoteTitle(voteData.title);
+                        setVoteImage1(voteData.voteImage1);
+                        setVoteImage2(voteData.voteImage2);
+                        setVoteItem1(voteData.voteItem1);
+                        setVoteItem2(voteData.voteItem2);
+                    }
+
+                } catch (error) {
+                    console.error("Error fetching data: ", error);
+                }
+            };
+
+            fetchData();
+        }, [accessToken]);
+
     return  (
         <div className='insta-balancegame-box'>
             <div className='insta-past-votes'>
                 <button className='insta-past-votes-button'>지난 투표</button>
             </div>
             <div className='insta-vote-header'>
-                <div className='insta-voting-topic'>싸우면 누가 이길까?</div>
+                <div className='insta-voting-topic'>{voteTitle}</div>
             </div>
             <div className='insta-share-box'>
                 <button>공유 하기</button>
             </div>
             <div className='insta-vote-item'>
                 <div className='insta-vote1'>
-                    <img className='insta-vote-frame1' src="https://png.pngtree.com/png-vector/20240206/ourmid/pngtree-iphone-15-pro-photocall-camera-vector-png-image_11719125.png"></img>                
-                    <div className='insta-vote-name1'>호랑이</div>
+                    <img className='insta-vote-frame1' src="https://png.pngtree.com/png-vector/20240206/ourmid/pngtree-iphone-15-pro-photocall-camera-vector-png-image_11719125.png"></img>
+                    <div className='insta-vote-name1'>{voteItem1}</div>
                     <button>투표</button>
-                    <img className='insta-vote-image1' src="https://gnews.gg.go.kr/OP_UPDATA/UP_DATA/_FILEZ/202112/20211227054431183687488.jpg"></img>
+                    <img className='insta-vote-image1' src={voteImage1}></img>
                 </div>
                 <div className='insta-vote2'>
                     <img className='insta-vote-frame2' src="https://png.pngtree.com/png-vector/20240206/ourmid/pngtree-iphone-15-pro-photocall-camera-vector-png-image_11719125.png"></img>
-                    <div className='insta-vote-name2'>사자</div>
+                    <div className='insta-vote-name2'>{voteItem2}</div>
                     <button>투표</button>
-                    <img className='insta-vote-image2' src="https://static.news.zumst.com/images/2/2020/01/01/6004e56f490341d88d4f022163af8c4b.jpg"></img>
+                    <img className='insta-vote-image2' src={voteImage2}></img>
                 </div>
             </div>
             <div className='insta-votebar'><InstaBalancebar vote1={200} vote2={100} /></div>
