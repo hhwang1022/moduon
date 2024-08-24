41a1494e2d78a53cafc009b0824e74afce4ecb6c
hhwang1022 <hjd2110@naver.com>
2024-08-24 14:43:24 +0900

    balancegamebar modify

--- src/main/frontend/src/component/BalenceBar.js
+++ src/main/frontend/src/component/BalenceBar.js
@@ -7,25 +7,45 @@ const BalenceBar = ({ left, right }) => {
 
   const per = Math.floor((left / (left + right)) * 100);
 
-  useEffect(() => {
+  const totalTime = 1000;
+    let stepTime = 0;
 
-    for (let i = 1; i < left; i++) {
-      (function (x) {
-        setTimeout(function () {
-          setleft(i);
-        }, 10 * x);
-      })(i);
+  useEffect(() => {
+    if (left > right) {
+      stepTime = totalTime / left;
+      for (let i = 1; i < left; i++) {
+        (function (x) {
+          setTimeout(function () {
+            setleft(i);
+          }, stepTime * x);
+        })(i);
+      }
     }
-    
+    else {
+      stepTime = totalTime / right;
+      for (let i = left + right; i >= left; i--) {
+        (function (x) {
+          setTimeout(function () {
+            setleft(i);
+          }, stepTime * (left + left - x));
+        })(i);
+      }
+    }
+
   }, [left]);
 
-  return (<progress
+  return (<span><progress
     class="progress"
     id="progress"
     value={leftpoint}
     min={0}
     max={left + right}
-  ></progress>);
+  ></progress>
+    <div className='balancepointbar'>
+      <div className={'balanceleftpoint'}>{left.toLocaleString()}</div>
+      <div className={'balancerighttpoint'}>{right.toLocaleString()}</div>
+    </div>
+  </span>);
 };
 
 export default BalenceBar;
