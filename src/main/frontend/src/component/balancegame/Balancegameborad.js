fd9d97c51da01900ae1fa306249eceec18d81830
wooil6 <wooilkf@naver.com>
2024-08-25 23:09:41 +0900

    Last BalanceGame reply

--- src/main/frontend/src/component/balancegame/Balancegameborad.js
+++ src/main/frontend/src/component/balancegame/Balancegameborad.js
@@ -1,22 +1,34 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Routes, Route, useNavigate, useParams } from 'react-router-dom';
 import Currentvote_board from '../currentvote/Currentvote_board';
 import InstaCurrentvote_board from '../instacurrentvote/InstaCurrentvote_board';
 import Balancegamelist from './Balancegamelist';
+import LastBalanceGame from './LastBalanceGame';
 import Balancegamewrite from './Balancegamewrite';
+
 
 const Balancegameboard = ({ generation }) => {
     const navigate = useNavigate();
     const { balanceid } = useParams();
+    const [balanceGameId, setbalanceGameId] = useState(balanceid ? parseInt(balanceid) : 1);
 
-    return (
-        <Routes>
-            <Route path="/" element={<Balancegamelist generation={generation} onClickwirtebtn={() => navigate('write')} />} />
-            <Route path="write" element={<Balancegamewrite />} />
-            <Route path="view/:balanceid" element={generation !== "1020" ? <Currentvote_board generation={generation} onclicklistbtn={() => navigate('/main_' + generation + '/balance')} /> : <InstaCurrentvote_board />} />
-            <Route path="view" element={generation !== "1020" ? <Currentvote_board generation={generation} onclicklistbtn={() => navigate('/main_' + generation + '/balance')} /> : <InstaCurrentvote_board />} />
-        </Routes>
-    );
+    useEffect(() => {
+        console.log("balanceGameId: " + balanceGameId );
+    }, [balanceGameId]);
+
+    const handleReadClick = (id) => {
+        setbalanceGameId(id);
+        navigate(`view/${id}`);
+    };
+
+     return (
+            <Routes>
+                <Route path="/" element={<Balancegamelist generation={generation}
+                    onClickwirtebtn={handleReadClick} />} />
+                <Route path="write" element={<Balancegamewrite />} />
+                <Route path="view/:balanceid" element={<LastBalanceGame generation={generation} balanceid={balanceGameId}/>} />
+                <Route path="view" element={generation !== "1020" ? <Currentvote_board generation={generation} onclicklistbtn={() => navigate('/main_' + generation + '/balance')} /> : <InstaCurrentvote_board />} />
+            </Routes>
+        );
 };
-
 export default Balancegameboard;
--- src/main/frontend/src/component/balancegame/LastBalanceGame.js
+++ src/main/frontend/src/component/balancegame/LastBalanceGame.js
@@ -1,117 +1,130 @@
 import './LastBalanceGame.css';
 import axios from 'axios';
-import BalanceBar from '../currentvote/BalanceBar'
-import React, { useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
-import Balancegame_commentlist from '../currentvote/Balancegame_commentlist';
-import memberInfo from '../../MemberInfo';
-import KakaoButton from '../KakaoButton';
-import FacebookButton from '../FacebookButton';
-import TwitterButton from '../TwitterButton';
-  const info = memberInfo.getMemberInfo();
-  const LastBalanceGame= ({generation }) => {
-    const {balanceid} = useParams();
-    const balanceGameId = balanceid;
-    // const [generation, setGeneration] = useState('');
-    const [voteTitle, setVoteTitle] = useState('');
-    const [voteImage1, setVoteImage1] = useState('');
-    const [voteImage2, setVoteImage2] = useState('');
-    const [voteItem1, setVoteItem1] = useState('');
-    const [voteItem2, setVoteItem2] = useState('');
-    const [searchkeyword, setsearchkeyword] = useState('');
-    const [commentList, setCommentList] = useState([]);
-    const [commentListUpdated, setCommentListUpdated] = useState(false);
-    const [votePoint1, setVotePoint1] = useState(0);
-    const [votePoint2, setVotePoint2] = useState(0);
-    const [voteItem, setVoteItem] = useState('');
-    const [votePageReset, setVotePageReset] = useState(false);
-    let accessToken = window.localStorage.getItem('accessToken');
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const response = await axios.get('http://127.0.0.1:8080/balancegames/' + balanceGameId, {
-            headers: {Authorization: `Bearer ${accessToken}`}
-          });
-          const data = response.data.data;
-          console.log(response);
-          if (data) {
-            const voteData = data;
-            console.log("fetched data: ", data);
-            setVoteTitle(voteData.title);
-            setVoteImage1(voteData.voteImage1);
-            setVoteImage2(voteData.voteImage2);
-            setVoteItem1(voteData.voteItem1);
-            setVoteItem2(voteData.voteItem2);
-           // setBalanceGameId(voteData.balanceGameId);
-            setVotePoint1(voteData.votePoint1);
-            setVotePoint2(voteData.votePoint2);
-          }
-          setVotePageReset(false);
-        } catch (error) {
-          console.error("Error fetching data: ", error);
+import BalanceBar from '../currentvote/BalanceBar';
+import React, { useState, useEffect, useRef } from 'react';
+import { useParams } from 'react-router-dom';
+import Balancegame_commentlistItem from '../currentvote/Balancegame_commentlistItem';
+
+const LastBalanceGame = ({ generation }) => {
+  const { balanceid } = useParams();
+  const balanceGameId = balanceid;
+  const [voteTitle, setVoteTitle] = useState('');
+  const [voteImage1, setVoteImage1] = useState('');
+  const [voteImage2, setVoteImage2] = useState('');
+  const [voteItem1, setVoteItem1] = useState('');
+  const [voteItem2, setVoteItem2] = useState('');
+  const [searchkeyword, setSearchkeyword] = useState('');
+  const [commentList, setCommentList] = useState([]);
+  const [commentListUpdated, setCommentListUpdated] = useState(false);
+  const [votePoint1, setVotePoint1] = useState(0);
+  const [votePoint2, setVotePoint2] = useState(0);
+  const [votePageReset, setVotePageReset] = useState(false);
+
+  let accessToken = window.localStorage.getItem('accessToken');
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://127.0.0.1:8080/balancegames/' + balanceGameId, {
+        headers: { Authorization: `Bearer ${accessToken}` }
+      });
+      const data = response.data.data;
+      console.log(response);
+
+      if (data) {
+        const voteData = data;
+        console.log("fetched data: ", data);
+        setVoteTitle(voteData.title);
+        setVoteImage1(voteData.voteImage1);
+        setVoteImage2(voteData.voteImage2);
+        setVoteItem1(voteData.voteItem1);
+        setVoteItem2(voteData.voteItem2);
+        setVotePoint1(voteData.votePoint1);
+        setVotePoint2(voteData.votePoint2);
+        setCommentList(voteData.commentList || []);
+      }
+      setVotePageReset(false);
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [commentListUpdated]);
+
+  const handlePostReply = async () => {
+    try {
+      const response = await axios.post(
+        'http://127.0.0.1:8080/balancegames/' + balanceGameId + '/reply',
+        { body: searchkeyword },
+        {
+          'Content-Type': 'application/json',
+          headers: { Authorization: `Bearer ${accessToken}` }
         }
-      };
-      fetchData();
-    }, [accessToken, generation, votePageReset, balanceGameId]);
-    const handlePostReply = async () => {
-      try {
-        const response = await axios.post(
-          'http://127.0.0.1:8080/balancegames/' + balanceGameId + '/reply',
-          {
-            body: searchkeyword,
-          },
-          {
-            'Content-Type': 'application/json',
-            headers: {
-              Authorization: `Bearer ${accessToken}`,
-            },
-          });
-        setsearchkeyword('');
-        setCommentListUpdated(true);
-      } catch (error) {
-        alert(JSON.stringify(error.message));
-      }
-    };
-    const handlePostVote = async (vote) => {
-      try {
-        const response = await axios.post(
-          'http://127.0.0.1:8080/balancegames/' + balanceGameId + '/vote',
-          {
-            voteItem: vote
-          },
-          {   'Content-Type': 'application/json',
-            headers: { Authorization: `Bearer ${accessToken}`,
-            },
-          });
-      } catch (error) {
-        alert(JSON.stringify(error.message));
-      }
-    };
-    return (
-      <div className='vote-mainbox'>
-        <div className='vote-header'>
-          <div className='voting-topic'> {voteTitle}</div>
+      );
+      setSearchkeyword('');
+      setCommentListUpdated(true);
+    } catch (error) {
+      console.error("Error posting reply:", error);
+      alert(JSON.stringify(error.message));
+      console.log(error.response.data);
+    }
+  };
+
+  const scrollableDivRef = useRef(null);
+
+  useEffect(() => {
+    fetchData();
+  }, [commentListUpdated]);
+
+  useEffect(() => {
+    const scrollableDiv = scrollableDivRef.current;
+    if (scrollableDiv) {
+      scrollableDiv.scrollTop = scrollableDiv.scrollHeight;
+    }
+  }, [commentList]);
+
+  useEffect(() => {
+    if (commentListUpdated) {
+      setCommentListUpdated(false);
+    }
+  }, [commentListUpdated]);
+
+  return (
+    <div className='vote-mainbox'>
+      <div className='vote-header'>
+        <div className='voting-topic'> {voteTitle}</div>
+      </div>
+      <div className='vote-box'>
+        <div className="vote-item">
+          <img className='vote-image' src={voteImage1} alt="Vote Option 1" />
+          <div className={'vote-name' + generation}>{voteItem1}</div>
         </div>
-        <div className='vote-box'>
-          <div className="vote-item">
-            <img className='vote-image' src={voteImage1}></img>
-            <div className={'vote-name' + generation}>{voteItem1}</div>
-          </div>
-          <div className='votebar'><BalanceBar vote1={votePoint1} vote2={votePoint2} generation={generation} /></div>
-          <div class="vote-item">
-            <img className='vote-image' src={voteImage2}></img>
-            <div className={'vote-name' + generation}>{voteItem2}</div>
-          </div>
+        <div className='votebar'>
+          <BalanceBar vote1={votePoint1} vote2={votePoint2} generation={generation} />
         </div>
-        <div className='comments-box'>
-          <div className='comment'><Balancegame_commentlist generation={generation} balanceGameId={balanceGameId}
-          commentListUpdated={commentListUpdated} setCommentListUpdated={setCommentListUpdated}/></div>
-          <div className='comment-form'>
-            <textarea className='comment-box' value={searchkeyword} onChange={(e) => setsearchkeyword(e.target.value)}></textarea>
-            <button className='comment-submit' onClick={handlePostReply}>등록</button>
-          </div>
+        <div className="vote-item">
+          <img className='vote-image' src={voteImage2} alt="Vote Option 2" />
+          <div className={'vote-name' + generation}>{voteItem2}</div>
         </div>
       </div>
-    );
-  };
+      <div className='comments-box'>
+        <div id='scrollableDiv' ref={scrollableDivRef} className='comment'>
+          {commentList.length > 0 ? (
+            commentList.map((x, index) => (
+              <Balancegame_commentlistItem key={index} comment={x} generation={generation} />
+            ))
+          ) : (
+            <p>No comments available.</p>
+          )}
+        </div>
+        <div className='comment-form'>
+          <textarea className='comment-box' value={searchkeyword} onChange={(e) => setSearchkeyword(e.target.value)}></textarea>
+          <button className='comment-submit' onClick={handlePostReply}>등록</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default LastBalanceGame;
