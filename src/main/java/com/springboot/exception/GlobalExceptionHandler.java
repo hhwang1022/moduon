0e707d7d2ccf136bdbea9596222c373e4ceeff24
hhwang1022 <hjd2110@naver.com>
2024-08-25 11:02:36 +0900

    에러핸들러 수정

--- src/main/java/com/springboot/exception/GlobalExceptionHandler.java
+++ src/main/java/com/springboot/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.springboot.exception;
 
-import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,28 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BusinessLogicException.class)
-    public ResponseEntity<ErrorResponse> handleBusinessLogicException(BusinessLogicException ex) {
+    public ResponseEntity<ExceptionCode> handleBusinessLogicException(BusinessLogicException ex) {
         ExceptionCode exceptionCode = ex.getExceptionCode();
-
-        ErrorResponse errorResponse = new ErrorResponse(
-                HttpStatus.BAD_REQUEST.value(),
-                exceptionCode.getMessage(),
-                exceptionCode.name()
-        );
-
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
-    }
-
-    @Getter
-    public class ErrorResponse {
-        private int status;
-        private String message;
-        private String code;
-
-        public ErrorResponse(int status, String message, String code) {
-            this.status = status;
-            this.message = message;
-            this.code = code;
-        }
+        return new ResponseEntity<>(exceptionCode, HttpStatus.BAD_REQUEST);
     }
 }
