3d1e0606e4c7f669ba51e7d9bd820866bb9abafe
hhwang1022 <hjd2110@naver.com>
2024-08-26 16:53:32 +0900

    투표 수정중

--- src/main/frontend/src/component/post/HotPostList.js
+++ src/main/frontend/src/component/post/HotPostList.js
@@ -20,7 +20,7 @@ const HotPostList = ({generation}) => {
 
         const fetchHotPosts = async () => {
             try {
-                const response = await axios.get(process.env.REACT_APP_API_URL + 'posts/popular?' + 'page=' + 1 + '&size=' + 10, {
+                const response = await axios.get(process.env.REACT_APP_API_URL + 'posts/popular?' + 'page=' + 1 + '&size=' + 6, {
                  headers: { Authorization: `Bearer ${accessToken}` }
                 });
                 if(response && response.data && response.data.data) {
