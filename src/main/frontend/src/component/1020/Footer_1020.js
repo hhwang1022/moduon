3dacaab243de68c266ec94c16a6211d6a50efcc4
wooil6 <wooilkf@naver.com>
2024-08-28 09:19:35 +0900

    이메일 형식 검증

--- src/main/frontend/src/component/1020/Footer_1020.js
+++ src/main/frontend/src/component/1020/Footer_1020.js
@@ -3,12 +3,6 @@ import './Footer_1020.css';
 const Footer_1020 = ({ title }) => {
   return (
     <div>
-      <div className="header">
-        <div className="left">
-          <img className="padding" src="https://cdn-icons-png.flaticon.com/512/25/25424.png" width={30} height={30} />
-          <img className="padding" src="https://cdn-icons-png.flaticon.com/512/109/109594.png" width={30} height={30} />
-          <img className="padding" src="https://cdn-icons-png.flaticon.com/512/13/13267.png" width={30} height={30} />
-        </div>
         <div className='middle'>
           <div className='balencegame_scroll_btncontent'>
             <div className="balencegame_scroll_btn_active"></div>
